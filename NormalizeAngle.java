ffba3fbb17e53307b68b7c56e713d8a1e47b2556
Ajah Dailey <ajahdailey@zipcoders-MBP-3.fios-router.home>
2019-02-06 23:20:53 -0500

    changes on lab

--- NormalizeAngle.java
+++ NormalizeAngle.java
@@ -4,11 +4,18 @@
 public class NormalizeAngle {
 
     public Integer normalizeValueUsingModulo(Integer angle){
-        return 0;
+        int newAngle = angle;
+        while (newAngle <= -100) newAngle += 360;
+        while (newAngle > 100) newAngle -=360;
+        return newAngle;
     }
 
     public Integer normalizeValueUsingFloorMod(Integer integer){
-        return 0;
+        integer %= 360;
+        if (integer < -100) integer += 360;
+        else if (integer > 100) integer -= 360;
+        
+        return integer;
     }
 
     public static void main(String[] args){
